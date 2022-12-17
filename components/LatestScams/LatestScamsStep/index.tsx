7b7bf8d3c73b382ff90bea9c29182e1545e09e94
Itsjustblack <jason.aghedo@gmail.com>
2022-12-17 13:39:35 +0100

    Feat: Made latest scams page responsive on mobile

--- components/LatestScams/LatestScamsStep/index.tsx
+++ components/LatestScams/LatestScamsStep/index.tsx
@@ -1,5 +1,5 @@
 interface LatestScamStepProps {
-	number: number;
+	number: string;
 	text: string;
 }
 
@@ -7,7 +7,7 @@ const LatestScamStep: React.FC<LatestScamStepProps> = ({ number, text }) => {
 	return (
 		<div className="flex flex-col gap-y-[18px] items-center">
 			<div className="w-11 h-11 rounded-full flex items-center justify-center bg-gradient-to-br from-[#0B63C5] to-[#073D79] text-2xl font-semibold text-white">{number}</div>
-			<p>{text}</p>
+			<p className="text-[14px] leading-[27px] sm:text-xl xl:leading-[36px]">{text}</p>
 		</div>
 	);
 };
